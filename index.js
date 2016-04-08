a4dcebfc64c656c6d8fa3a11f83c18ed92d0dd6e
M. Elliot Frost <melliotfrost@gmail.com>
2016-04-08 11:25:01 -0400

    Load seed function async

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ var seed = require(path.join(libPath, 'seed')),
 module.exports = function(sails){
   return {
     initialize: function(done){
-      // first
-      //
-      // patch model to acquire seed data attribute
-      sails
-        .after(['hook:moduleloader:loaded'], function(){
-          patchAttributes();
-        });
-
       //later on wait for this/these event(s)
       //to apply methods to models
       var eventsToWaitFor = [];
@@ -40,8 +32,11 @@ module.exports = function(sails){
           //then seed models
           //and let sails continue
 
-          patch();
-          seeds(done);
+          patchAttributes(function(){
+            patch(function(){);
+              seeds(done);
+            });
+          });
         });
     }
   };
@@ -51,8 +46,8 @@ function seeds(callback){
   if(sails.config.seeds.disable){
     callback()
   } else {
-    async.eachSeries(Object.keys(sails.models), function(model, cb){
-      if(sails.models[model].seed && !sails.config.seeds[model].active === false){
+    async.each(_.keys(sails.models), function(model, cb){
+      if(sails.models[model].seed && !(sails.config.seeds[model].active === false)){
         sails.models[model].seed(cb);
       } else {
         cb();
@@ -64,20 +59,23 @@ function seeds(callback){
     });
   }
 };
-function patch(){
-  _(sails.models)
-    .forEach(function(model){
-      if(model.globalId){
-        seed(model);
-        seedArray(model);
-        seedObject(model);
-      }
-    });
+function patch(cb){
+  async.each(_.toArray(sails.models), function(model, callback){
+    if(model.globalId){
+      seed(model);
+      seedArray(model);
+      seedObject(model);
+      callback();
+    } else {
+      callback();
+    }
+  }, function()[
+    cb();
+  });
 }
 
-function patchAttributes(){
-  _(sails.models)
-    .forEach(function(model){
+function patchAttributes(callback){
+  async.each(_.toArray(sails.models), function(model, cb){
       var data = sails.config.seeds[model.identity];
       if(data){
         var extend = {};
@@ -91,11 +89,15 @@ function patchAttributes(){
         }
 
         _.extend(model, extend);
+        cb();
 
       } else {
         _.extend(model, {
           seedData: null
         });
+        cb();
       }
+    }, function(){
+      callback();
     });
 }
